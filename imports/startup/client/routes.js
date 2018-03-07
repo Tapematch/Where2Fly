aab708c20576961378de33a2eb88ed7966cf9ad3
Phil Taubert <phil.taubert@stud.htwk-leipzig.de>
2018-03-07 15:16:40 +0100

    Anpassungen im Aufbau, Merge mit Profilseite

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,6 +2,7 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import '../../ui/header/header.js';
 import '../../ui/footer/footer.js';
+import '../../ui/detail/detail.js';
 import '../../ui/map/map.js';
 import '../../ui/placelist/placelist.js';
 import '../../ui/profile/profile.js';
@@ -10,7 +11,7 @@ FlowRouter.route('/', {
     name: 'home',
     action() {
         console.log("Home");
-        BlazeLayout.render('body', { header: 'header', body: 'map', footer: 'footer' });
+        BlazeLayout.render('body', {header: 'header', body: 'map', footer: 'footer'});
     },
 });
 
@@ -27,5 +28,13 @@ FlowRouter.route('/profile', {
     action() {
         console.log("profile");
         BlazeLayout.render('body', { header: 'header', body: 'profile', footer: 'footer' });
+    },
+});
+
+FlowRouter.route('/place/:_id', {
+    name: 'details',
+    action() {
+        console.log("detail");
+        BlazeLayout.render('body', {header: 'header', body: 'detail', footer: 'footer'});
     },
 });
