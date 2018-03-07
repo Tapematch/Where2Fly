a3bd0e0e8378b85e337f1882ac341a1804e45fbc
DaniMert <danielmertens91@gmail.com>
2018-03-07 13:37:21 +0100

    Detailseiten

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,6 +2,7 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import '../../ui/header/header.js';
 import '../../ui/footer/footer.js';
+import '../../ui/detail/detail.js';
 import '../../ui/map/map.js';
 import '../../ui/placelist/placelist.js';
 
@@ -9,7 +10,7 @@ FlowRouter.route('/', {
     name: 'home',
     action() {
         console.log("Home");
-        BlazeLayout.render('body', { header: 'header', body: 'map', footer: 'footer' });
+        BlazeLayout.render('body', {header: 'header', heroimage: 'map', body: '', footer: 'footer'});
     },
 });
 
@@ -18,5 +19,13 @@ FlowRouter.route('/places', {
     action() {
         console.log("placelist");
         BlazeLayout.render('body', { header: 'header', body: 'placelist', footer: 'footer' });
+    },
+});
+
+FlowRouter.route('/place/:_id', {
+    name: 'details',
+    action() {
+        console.log("detail");
+        BlazeLayout.render('body', {header: 'header', heroimage: 'map', body: 'detail', footer: 'footer'});
     },
 });
