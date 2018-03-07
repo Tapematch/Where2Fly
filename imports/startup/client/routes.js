5210faea61f2dc1a0268d7f8853a58611b3a0424
Phil Taubert <phil.taubert@stud.htwk-leipzig.de>
2018-03-07 12:57:36 +0100

    Nutzerprofile vorbereitet

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -4,6 +4,7 @@ import '../../ui/header/header.js';
 import '../../ui/footer/footer.js';
 import '../../ui/map/map.js';
 import '../../ui/placelist/placelist.js';
+import '../../ui/profile/profile.js';
 
 FlowRouter.route('/', {
     name: 'home',
@@ -18,5 +19,13 @@ FlowRouter.route('/places', {
     action() {
         console.log("placelist");
         BlazeLayout.render('body', { header: 'header', body: 'placelist', footer: 'footer' });
+    },
+});
+
+FlowRouter.route('/profile', {
+    name: 'profile',
+    action() {
+        console.log("profile");
+        BlazeLayout.render('body', { header: 'header', body: 'profile', footer: 'footer' });
     },
 });
