dae0b174b0210a84e13b73cf2d6def9392887916
Phil Taubert <phil.taubert@stud.htwk-leipzig.de>
2018-03-08 17:00:14 +0100

    Profilseiten, auf Bootstrap Accounts umgestellt

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,17 +1,35 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import '../../ui/header/header.js';
+import '../../ui/login/login.js';
 import '../../ui/footer/footer.js';
 import '../../ui/detail/detail.js';
 import '../../ui/map/map.js';
 import '../../ui/placelist/placelist.js';
-import '../../ui/profile/myprofile.js';
+import '../../ui/profile/profile.js';
 
 FlowRouter.route('/', {
     name: 'home',
     action() {
         console.log("Home");
         BlazeLayout.render('body', {header: 'header', body: 'map', footer: 'footer'});
+    },
+});
+
+FlowRouter.route('/login', {
+    name: 'login',
+    action() {
+        console.log("Login");
+        BlazeLayout.render('body', {header: 'header', body: 'login', footer: 'footer'});
+    },
+});
+
+FlowRouter.route('/logout', {
+    name: 'login',
+    action() {
+        console.log("Logout");
+        Meteor.logout();
+        FlowRouter.go('/');
     },
 });
 
@@ -27,7 +45,7 @@ FlowRouter.route('/profile', {
     name: 'profile',
     action() {
         console.log("profile");
-        BlazeLayout.render('body', { header: 'header', body: 'myprofile', footer: 'footer' });
+        BlazeLayout.render('body', { header: 'header', body: 'profile', footer: 'footer' });
     },
 });
 
